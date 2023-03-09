e92e3ba382833b13df5772def3cb54207636b5a5
ccgsgcc <qweoctave@gmail.com>
2023-03-09 23:42:26 +0600

    Initial commit

---
+++ src/variables.java
@@ -0,0 +1,15 @@
+//2
+public class variables {
+    public static void main(String[] args){
+        int myInt;
+        myInt = 557;
+         short myShort = 32000;
+          long myLong = 21313123;
+           double myDouble = 235.22;
+            float myFloat = 2222.22f;
+             char c = 'a';
+              boolean b = true;
+               byte myByte = 100;
+        System.out.println(myInt);
+    }
+}
