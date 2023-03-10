e0a3c7da452c83db9cdb6588b1536534b7678cad
ccgsgcc <qweoctave@gmail.com>
2023-03-10 13:49:05 +0600

    10.03.23

---
+++ src/ForLoop.java
@@ -0,0 +1,12 @@
+//5 for цикл
+public class ForLoop {
+    public static void main(String [] args) {
+        for (int i = 0;i <= 10;i++){
+            System.out.println("Hello! "+ i);
+            // i++ увеличвается ( i = i+1 (true);
+            // 1 i это первое значение,чему равняется,
+            // второе до сколько работать
+
+        }
+    }
+}
